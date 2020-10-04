08337a544388beee3cb082dc96ea49502131e42d
konflic <konflic@mail.ru>
2020-10-04 18:04:25 +0300

    Версия 2.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
     parser.addoption("--app_host", default="http://192.168.1.73")
-    parser.addoption("--selenoid", default="192.168.1.73")
+    parser.addoption("--executor", default="192.168.1.73")
 
 
 @pytest.fixture
@@ -13,15 +13,11 @@ def browser(request):
 
     bro = request.config.getoption("--browser")
     app_host = request.config.getoption("--app_host")
-    selenoid = request.config.getoption("--selenoid")
+    executor = request.config.getoption("--executor")
 
-    caps = {"browserName": bro,
-            "enableVnc": True,
-            "enableVideo": True,
-            "enableLog": True,
-            "name": "QAPython"}
+    caps = {"browserName": bro}
 
-    wd = webdriver.Remote(command_executor=f"http://{selenoid}:4444/wd/hub", desired_capabilities=caps)
+    wd = webdriver.Remote(command_executor=f"http://{executor}:4444/wd/hub", desired_capabilities=caps)
     request.addfinalizer(wd.quit)
     wd.get(app_host)
     return wd
